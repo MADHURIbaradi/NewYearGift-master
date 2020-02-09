5a6e9e75ced16a175d5cd5ba6961cf757047d89e
madhuri reddy <baradimadhuri@gmail.com>
2020-02-09 20:44:11 +0530

    new-year-gift assignment

---
+++ Project2/src/main/java/com/madhuri/Project2/Chocolate.java
@@ -0,0 +1,14 @@
+package com.madhuri.Project2;
+
+import com.madhuri.Project2.sweet;
+
+public class Chocolate extends sweet{
+    Chocolate(String name, int weight, int cost) {
+		super(name, weight, cost);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected String getType(){
+  	  return "chocolate";
+    }
+}
